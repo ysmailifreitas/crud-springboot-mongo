e3eb43453a1f28302559814b97e0fc0302c3c43d
ysmaili.freitas <ysmaili.freitas@azi.com.br>
2022-08-11 09:36:30 -0500

    ClienteServiceImpl

--- src/main/java/com/meudominio/crud/api/services/impl/ClienteServiceImpl.java
+++ src/main/java/com/meudominio/crud/api/services/impl/ClienteServiceImpl.java
@@ -2,39 +2,42 @@ package com.meudominio.crud.api.services.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.meudominio.crud.api.documents.Cliente;
+import com.meudominio.crud.api.repositories.ClienteRepository;
 import com.meudominio.crud.api.services.ClienteService;
 
+@Service
 public class ClienteServiceImpl implements ClienteService {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 
 	@Override
 	public List<Cliente> listarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.clienteRepository.findAll();
 	}
 
 	@Override
 	public Cliente listarPorId(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.clienteRepository.findOne(id);
 	}
-
+	
 	@Override
 	public Cliente cadastrar(Cliente cliente) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.clienteRepository.save(cliente);
 	}
 
 	@Override
 	public Cliente atualizar(Cliente cliente) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.clienteRepository.save(cliente);
 	}
 
 	@Override
 	public void remover(String id) {
-		// TODO Auto-generated method stub
-
+		return this.clienteRepository.delete(id);
 	}
 
 }
